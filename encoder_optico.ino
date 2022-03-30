3b2e4f6722f4e04627b9e5e2edb3d96b4cbcd5db
Lazaroo3 <99274470+Lazaroo3@users.noreply.github.com>
2022-03-30 10:37:58 +0200

    Add files via upload

---
+++ encoder_optico.ino
@@ -0,0 +1,19 @@
+const int sensorPin = 9;
+long contador = 0;
+
+void setup(){
+	Serial.begin(9600);
+  	pinMode(sensorPin , INPUT);  //definir pin como entrada
+}
+
+void loop(){
+	int value = 0;
+  	value = digitalRead(sensorPin);  //lectura digital de Pin
+  	
+  if(value==LOW){
+  	Serial.print("activacion de contador -->");
+    contador=contador+1;
+    Serial.println(contador)
+  }
+  delay(10)
+}
